3a02c066b9e855e25bf51a87e715a3014d3640a6
Yicon <kaseki@126.com>
2022-10-28 15:05:28 +0800

    Update socket_server.py

--- network_new/socket_server.py
+++ network_new/socket_server.py
@@ -39,7 +39,8 @@ class NetworkServer (Process):
         print("my IP is " + self.ip)
  
         def _handler(sock, address):
-            jid = self._address_to_id(address)
+            #jid = self._address_to_id(address)
+            jid = self.id
             self.logger.info('hava recv jid %s' % (str(jid)))
             buf = b''
             try:
